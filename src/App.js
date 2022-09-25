1280e5e78a331a024d3710cd25a1d518ea7a97dd
Maxim Derboven <maxim.derboven@student.kdg.be>
2022-09-25 19:13:08 +0200

    Deel 1 - Layout, list, play sound

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import logo from './logo.svg';
 import './App.css';
+
+function playSound(e) {
+  new Audio(e).play();
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="soundboard">
+        {data.items.map((item) => (
+          <div
+            className="item"
+            key={item.id}
+            onClick={() => playSound(item.sound)}
+          >
+            <img alt={item.name} src={item.image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+const data = {
+  items: [
+    {
+      id: 1,
+      name: "de naam van dit item",
+      image: "https://url-naar-image-van-dit-item",
+      sound: "https://url-naar-geluid-van-dit-item",
+    },
+    {
+      id: 2,
+      name: "de naam van dit item",
+      image: "https://url-naar-image-van-dit-item",
+      sound: "https://url-naar-geluid-van-dit-item",
+    },
+  ],
+};
 
 export default App;
